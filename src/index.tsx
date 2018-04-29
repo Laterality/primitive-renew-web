944015a0854cb074432768c6c787087918933f6e
Shin Jinwoo <bluesky0255@naver.com>
2018-04-29 14:15:23 +0900

    EDIT index.tsx
    - Add react-redux's Provider component to using redux store

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,17 @@
  */
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import * as Router from "react-router-dom";
 
 import { App } from "./App";
+import { store } from "./store/index";
 
 ReactDOM.render(
-	<Router.BrowserRouter>
-		<App/>
-	</Router.BrowserRouter>,
+	<Provider store={store}>
+		<Router.BrowserRouter>
+			<App/>
+		</Router.BrowserRouter>
+	</Provider>,
 	document.getElementById("root"),
 );
