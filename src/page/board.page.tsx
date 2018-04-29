a3c4522a06ed81d11d3b78ecdbfe545e513a27f2
Shin Jinwoo <bluesky0255@naver.com>
2018-04-29 22:53:30 +0900

    EDIT Board page component
    - It connected with react-redux
    - Code loading posts are moved here from Post list component

--- src/page/board.page.tsx
+++ src/page/board.page.tsx
@@ -4,40 +4,51 @@
  * author: Jinwoo Shin
  * date: 2018-04-16
  */
-import * as PropTypes from "prop-types";
+import * as axios from "axios";
 import * as query from "query-string";
 import * as React from "react";
+import { connect } from "react-redux";
 import * as ReactRouter from "react-router-dom";
+import { Dispatch } from "redux";
 
-import * as axios from "axios";
-
-import * as propTypes from "prop-types";
 import * as reqPost from "../lib/post.request";
 import * as reqUser from "../lib/user.request";
 
-import { onComponentReady } from "../lib/component-ready";
+import { BoardTitle, PostObject } from "../lib/post.obj";
+import { UserObject } from "../lib/user.obj";
 
+import { ObjectFactory } from "../lib/object-factory";
+
+import { onComponentReady } from "../lib/component-ready";
+import { ISessionVerifiable } from "../lib/session-verfying.interface";
+
+import { Button } from "../component/button.component";
 import { PostList } from "../component/post-list.component";
 
-export interface IBoardPageProps {
-	title: string;
-	page: number;
+import { UserActionCreator } from "../action/user.action";
+
+import { IStore } from "../store";
+
+export interface IBoardPageProps extends ISessionVerifiable {
 	history: any;
 	location: any;
+	user: UserObject | undefined;
 }
 
 export interface IBoardPageState {
 	title: string;
 	page: number;
+	posts: PostObject[];
 }
 
-export class BoardPage extends React.Component<IBoardPageProps, IBoardPageState> {
+class BoardPage extends React.Component<IBoardPageProps, IBoardPageState> {
 
-	public constructor(props: any) {
+	public constructor(props: IBoardPageProps) {
 		super(props);
 		this.state = {
+			title: BoardTitle.seminar,
 			page: 1,
-			title: "세미나",
+			posts: [],
 		};
 	}
 
@@ -45,9 +56,43 @@ export class BoardPage extends React.Component<IBoardPageProps, IBoardPageState>
 		onComponentReady();
 
 		const queries = query.parse(location.search);
+		this.setState({
+			title: queries["title"] || BoardTitle.seminar,
+			page: queries["page"] || 1});
 
-		this.checkLogin();
-		this.forceUpdate();
+		if (!this.props.user) {
+			reqUser.UserAPIRequest.checkSignedIn()
+			.then((res: axios.AxiosResponse) => {
+				const body = res.data;
+				if (body["state"]["signed"]) {
+					this.props.onSessionVerified(body["state"]["user"]);
+				}
+				else {
+					alert("로그인이 필요합니다.");
+					this.props.history["push"]("/");
+				}
+			});
+		}
+
+		reqPost.PostAPIRequest.retrievePostList(this.state.page, 
+			new Date().getFullYear(), this.state.title)
+		.then((res: axios.AxiosResponse) => {
+			if (res.status === 200) {
+				const body = res.data;
+				const posts: PostObject[] = [];
+
+				for (const p of body["posts"]["posts"]) {
+					posts.push(ObjectFactory.createPostObject(p));
+				}
+				this.setState({posts});
+			}
+			else {
+				alert("게시물 목록을 가져오는 데 실패했습니다.");
+			}
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 	}
 
 	public render() {
@@ -55,25 +100,14 @@ export class BoardPage extends React.Component<IBoardPageProps, IBoardPageState>
 			<div>
 				<nav className="navbar"></nav>
 				<h1 className="board-title">{this.state.title}</h1>
-				<PostList title={this.state.title} page={this.state.page} />
+				<PostList posts={this.state.posts} onItemClick={this.onPostClick}/>
 				<ReactRouter.Link to="/" >Home</ReactRouter.Link>
 				<ReactRouter.Link to="/write">
-				<button className="btn">글쓰기</button>
+				<Button text="글쓰기" iconSrc="/img/ic_create_white_48px.svg"/>
 				</ReactRouter.Link>
 				<button className="btn" onClick={this.onLogout}>logout</button>
 			</div>
 		);
-	}
-
-	private checkLogin = () => {
-		reqUser.UserAPIRequest.checkSignedIn()
-		.then(async (res: axios.AxiosResponse) => {
-			const body = res.data;
-			if (!body["state"]["signed"]) {
-				alert("로그인이 필요합니다.");
-				this.props.history["push"]("/");
-			}
-		});
 	}
 
 	private onLogout = () => {
@@ -86,4 +120,24 @@ export class BoardPage extends React.Component<IBoardPageProps, IBoardPageState>
 			}
 		});
 	}
+
+	private onPostClick = (id: string | number) => {
+		console.log("clicked post: ", id);
+	}
 }
+
+const mapStateToProps = (state: IStore) => {
+	return {
+		user: state.user,
+	};
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+	return {
+		onSessionVerified: (user: UserObject) => {
+			dispatch(UserActionCreator.setUser(user));
+		},
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BoardPage);
