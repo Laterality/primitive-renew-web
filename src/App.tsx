02c22fa23be83a1dae4d66d4b7d19bb842630f9c
Shin Jinwoo <bluesky0255@naver.com>
2018-04-29 22:48:40 +0900

    EDIT App component
    - Added Provider component to using redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,48 @@
+import { AxiosResponse } from "axios";
 import * as React from "react";
+import { connect, Provider } from "react-redux";
 import * as Router from "react-router-dom";
+import { Dispatch } from "redux";
 
-import { BoardPage } from "./page/board.page";
-import { HomePage } from "./page/home.page";
-import { WritePostPage } from "./page/write-post.page";
+import { default as BoardPage } from "./page/board.page";
+import { default as HomePage } from "./page/home.page";
+import { default as WritePostPage } from "./page/write-post.page";
 
 import { Routes } from "./routes";
 
+import { UserActionCreator } from "./action/user.action";
+
+import { UserAPIRequest } from "./lib/user.request";
+
+import { ObjectFactory } from "./lib/object-factory";
+import { UserObject } from "./lib/user.obj";
+
+import { store } from "./store/index";
+
 export class App extends React.Component {
+
+	public componentWillReceiveProps() {
+		UserAPIRequest.checkSignedIn()
+		.then((res: AxiosResponse) => {
+			const body = res.data;
+			if (body["state"]["signed"]) {
+				store.dispatch(UserActionCreator.setUser(ObjectFactory.createUserObject(body["state"]["user"])));
+			}
+			else { store.dispatch(UserActionCreator.setUser(undefined)); }
+		});
+	}
 
 	public render() {
 		return (
-			<div className="router-wrapper">
-				<Router.Route exact path={Routes.routeRoot} component={HomePage} />
-				<Router.Route path={Routes.routeBoard} component={BoardPage} />
-				<Router.Route path={Routes.routeWrite} component={WritePostPage} />
-			</div>
+			<Provider store={store}>
+				<Router.BrowserRouter>
+					<div className="router-wrapper">
+						<Router.Route exact path={Routes.routeRoot} component={HomePage} />
+						<Router.Route path={Routes.routeBoard} component={BoardPage} />
+						<Router.Route path={Routes.routeWrite} component={WritePostPage} />
+					</div>
+				</Router.BrowserRouter>
+			</Provider>
 		);
 	}
 }
