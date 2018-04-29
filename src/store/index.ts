8541ab12a6f39c0dfe1a4fc9e850965b8e09c3a8
Shin Jinwoo <bluesky0255@naver.com>
2018-04-29 13:55:40 +0900

    EDIT redux store to using redux-thunk as middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,6 +5,7 @@
  * date: 2018-04-28
  */
 import * as redux from "redux";
+import * as reduxThunk from "redux-thunk";
 
 import { BoardTitle, PostObject } from "../lib/post.obj";
 import { UserObject } from "../lib/user.obj";
@@ -15,4 +16,6 @@ export interface IStore {
 	posts: {[k in BoardTitle]: PostObject[]};
 }
 
-export const store = redux.createStore(reduce);
+export const store = redux.createStore(reduce,
+	redux.applyMiddleware(reduxThunk.default),
+);
