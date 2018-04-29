3a342a51fa8bf70ef7fbcfae1f69d718fe485db4
Shin Jinwoo <bluesky0255@naver.com>
2018-04-29 22:54:27 +0900

    EDIT Home page component
    - It is connected by react-redux
    - Session checking process changed to using ISessionVerifiable

--- src/page/home.page.tsx
+++ src/page/home.page.tsx
@@ -6,30 +6,50 @@
  */
 import * as axios from "axios";
 import * as React from "react";
+import { connect } from "react-redux";
+import * as Redux from "redux";
 
 import { onComponentReady } from "../lib/component-ready";
+import { ISessionVerifiable } from "../lib/session-verfying.interface";
 
 import { LoginForm } from "../component/login-form.component";
 
+import { ObjectFactory } from "../lib/object-factory";
+
 import { UserAPIRequest } from "../lib/user.request";
 
-export interface IHomePageProps {
+import { UserObject } from "../lib/user.obj";
+
+import { UserActionCreator } from "../action/user.action";
+
+import { IStore } from "../store";
+
+export interface IHomePageProps extends ISessionVerifiable {
 	history: any;
+	onLoginSucceed: (user: UserObject) => void;
+	user: UserObject | undefined;
 }
 
-export class HomePage extends React.Component<IHomePageProps> {
+class HomePage extends React.Component<IHomePageProps> {
 
 	public componentDidMount() {
-		// 세션 검사해서 로그인여부 확인
-		UserAPIRequest.checkSignedIn()
-		.then(async (res: axios.AxiosResponse) => {
-			const body = res.data;
-
-			if (body["state"]["signed"] === true) {
-				alert("이미 로그인되어있습니다.");
-				this.props.history["push"]("/board");
-			}
-		});
+		onComponentReady();
+		// 스토어로부터 로그인 여부 확인
+		if (this.props.user) {
+			alert("이미 로그인되어있습니다.");
+			this.props.history["push"]("/board");
+		}
+		else {
+			UserAPIRequest.checkSignedIn()
+			.then((res: axios.AxiosResponse) => {
+				const body = res.data;
+				if (body["state"]["signed"]) {
+					this.props.onSessionVerified(ObjectFactory.createUserObject(body["state"]["user"]));
+					alert("이미 로그인되어있습니다.");
+					this.props.history["push"]("/board");
+				}
+			});
+		}
 	}
 
 	public render() {
@@ -50,6 +70,7 @@ export class HomePage extends React.Component<IHomePageProps> {
 			const body = res.data;
 			if (body["result"] === "ok") {
 				// 로그인 성공
+				this.props.onLoginSucceed(ObjectFactory.createUserObject(body["user"]));
 				this.props.history["push"]("/board");
 			}
 			else {
@@ -63,3 +84,22 @@ export class HomePage extends React.Component<IHomePageProps> {
 		});
 	}
 }
+
+const mapStateToProps = (state: IStore) => {
+	return {
+		user: state.user,
+	};
+};
+
+const mapDispatchToProps = (dispatch: Redux.Dispatch) => {
+	return {
+		onLoginSucceed: (user: UserObject) => {
+			dispatch(UserActionCreator.setUser(user));
+		},
+		onSessionVerified: (user: UserObject) => {
+			dispatch(UserActionCreator.setUser(user));
+		},
+	};
+};
+
+export default connect(null, mapDispatchToProps)(HomePage);
