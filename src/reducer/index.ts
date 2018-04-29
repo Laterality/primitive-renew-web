39af603f9f97ccfe1589ea7996928811e4c32755
Shin Jinwoo <bluesky0255@naver.com>
2018-04-29 21:26:50 +0900

    REMOVE post from redux store and reducer
    - As now, the Post reducer and object stored in redux store is unnecessary.

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -18,19 +18,11 @@ import { UserActionType } from "../action/user.action";
 
 const initialState: IStore = {
 	user: undefined,
-	posts: {
-		[BoardTitle.seminar]: [],
-		[BoardTitle.works]: [],
-		[BoardTitle.jrBoard]: [],
-	},
 };
 
 export const reduce = (state = initialState, action: AnyAction): IStore => {
 	if (action.type in UserActionType) {
 		return userReducer(state, action);
-	}
-	else if (action.type in PostActionType) {
-		return postReducer(state, action);
 	}
 	else { return state; }
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,6 @@
  * date: 2018-04-28
  */
 import * as redux from "redux";
-import * as reduxThunk from "redux-thunk";
 
 import { BoardTitle, PostObject } from "../lib/post.obj";
 import { UserObject } from "../lib/user.obj";
@@ -13,9 +12,6 @@ import { reduce } from "../reducer";
 
 export interface IStore {
 	user: UserObject | undefined;
-	posts: {[k in BoardTitle]: PostObject[]};
 }
 
-export const store = redux.createStore(reduce,
-	redux.applyMiddleware(reduxThunk.default),
-);
+export const store = redux.createStore(reduce);
