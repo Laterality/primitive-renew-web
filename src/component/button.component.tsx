62ec31b99d80c05532d6cc813692c882183550a6
Shin Jinwoo <bluesky0255@naver.com>
2018-04-29 22:48:58 +0900

    ADD common Button component

---
+++ src/component/button.component.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+export interface IButtonProps {
+	iconSrc: string;
+	text: string;
+	onClick?: () => void | undefined;
+}
+
+export class Button extends React.Component<IButtonProps> {
+	public render() {
+		return (<button className="btn bg-primary text-white text-center"><img src={this.props.iconSrc} alt="button" className="icon mr-2" onClick={this.props.onClick}/>{this.props.text}</button>);
+	}
+}
