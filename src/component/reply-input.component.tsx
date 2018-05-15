b836f46d96c423223f244ba354c3d78feaa7e271
Shin Jinwoo <bluesky0255@naver.com>
2018-05-15 15:51:27 +0900

    EDIT reply input component
    - Changed button to my button component.
    - Added file description comment.

--- src/component/reply-input.component.tsx
+++ src/component/reply-input.component.tsx
@@ -1,5 +1,13 @@
+/**
+ * Reply input box component
+ * 
+ * author: Jinwoo Shin
+ * date: 2018-05-16
+ */
 import * as jquery from "jquery";
 import * as React from "react";
+
+import { Button } from "./button.component";
 
 export interface IReplyInputProps {
 	onInputClick: (input: string) => void;
@@ -11,7 +19,7 @@ export class ReplyInput extends React.Component<IReplyInputProps> {
 		return (
 		<div className="form-group">
 			<textarea className="form-control" id="reply-input" rows={5} placeholder="댓글 입력"/>
-			<button className="btn bg-primary" onClick={ () => this.onInputClick() }>댓글 달기</button>
+			<Button onClick={ () => this.onInputClick() } text="댓글 달기" />
 		</div>);
 	}
 
